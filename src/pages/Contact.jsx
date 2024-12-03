72eece04b032d04b8b545d4940e35c99596849de
adeeb786-code <adeebdigi786@gmail.com>
2024-12-03 16:35:33 +0530

    Ad0

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
     setStatus('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/send-whatsapp', {
+      const response = await fetch('https://firstdubai.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
